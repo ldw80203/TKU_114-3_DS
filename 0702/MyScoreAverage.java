10ee05f77ec549510148ed2a0ed8d6fe18cc350c
ldw80203 <ldw80203@gmail.com>
2026-07-02 10:43:03 +0800

    Add MyScoreAverage.java

---
+++ 0702/MyScoreAverage.java
@@ -0,0 +1,9 @@
+public class MyScoreAverage {
+    public static void main(String[] args) {
+        double Javascore = 85;
+        double Englishscore = 78;
+        double Mathscore = 92;
+        double average = ( Javascore + Englishscore + Mathscore) / 3;
+        System.out.println("Average score: " + average);
+    }
+}
