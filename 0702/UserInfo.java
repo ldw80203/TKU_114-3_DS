cf52536f3c94fa7852dd663a0838412c85557400
ldw80203 <ldw80203@gmail.com>
2026-07-02 10:30:54 +0800

    Add UserInfo.java

---
+++ 0702/UserInfo.java
@@ -0,0 +1,12 @@
+public class UserInfo {
+    public static void main(String[] args) {
+        String userName = "William";
+        int userId = 410630536;
+        String department = "Information Management";
+        int expectedScore = 100;
+        System.out.println("User Name: " + userName);
+        System.out.println("User ID: " + userId);   
+        System.out.println("Department: " + department);
+        System.out.println("Expected Score: " + expectedScore);
+    }
+}
