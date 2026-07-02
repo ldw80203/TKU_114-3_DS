4c1aa3c2275665c56738fbec32ce5a3f810243e9
ldw80203 <ldw80203@gmail.com>
2026-07-02 10:23:26 +0800

    Add ProfileCard.java

---
+++ 0702/ProfileCard.java
@@ -0,0 +1,11 @@
+public class ProfileCard {
+    public static void main(String[] args) {
+        String name = "William";
+        String department = "Information Management";
+        String favorite_course = "English";
+
+
+        System.out.println("Name: "+ name);
+        System.out.println("Department: "+ department);
+        System.out.println("Favorite Course: "+ favorite_course);
+    }}
