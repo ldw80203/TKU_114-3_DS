0a47d9d110d0ceccf2694f3ac2b96d41f2b5cd57
ldw80203 <ldw80203@gmail.com>
2026-07-02 10:37:04 +0800

    Add CourseInfo.java

---
+++ 0702/CourseInfo.java
@@ -0,0 +1,12 @@
+public class CourseInfo {
+    public static void main(String[] args) {
+        String courseName = "Programming and Data structure";
+        int credits = 3;
+        int hours = 45;
+        boolean required = true;
+        System.out.println("課程名稱: " + courseName);
+        System.out.println("學分數: " + credits);
+        System.out.println("上課時數: " + hours);
+        System.out.println("必修: " + required);
+    }
+}
