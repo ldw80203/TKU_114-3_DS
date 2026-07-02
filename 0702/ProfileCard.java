680de034ffd56e9bb77a9e66c624f8b79486634f
ldw80203 <ldw80203@gmail.com>
2026-07-02 10:27:14 +0800

    Add comment on ProfileCard.java

--- 0702/ProfileCard.java
+++ 0702/ProfileCard.java
@@ -3,9 +3,9 @@ public class ProfileCard {
         String name = "William";
         String department = "Information Management";
         String favorite_course = "English";
+        //定義3個字串變數，分別為姓名、系所、最喜歡的課程
 
-
-        System.out.println("Name: "+ name);
-        System.out.println("Department: "+ department);
-        System.out.println("Favorite Course: "+ favorite_course);
+        System.out.println("Name: "+ name);//輸出姓名
+        System.out.println("Department: "+ department);//輸出系所
+        System.out.println("Favorite Course: "+ favorite_course);//輸出最喜歡的課程
     }}
